5c47ab26c27b1c032973d80da37d42f60b8a006c
Lucas Cardoso de Castro <sky.lucas2013@gmail.com>
2021-04-30 17:23:17 -0300

    correcao prototype

--- interfaces/PrototypeMensagem.php
+++ interfaces/PrototypeMensagem.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-    Fazendo uso do padrão prototype para implementar clones para as mensagens
+    Fazendo uso do padrão prototype para implementar clones para as mensagens (Pode ser observado em AcoesMensagens)
 */
 interface PrototypeMensagem
 {
